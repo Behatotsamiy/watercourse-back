405585ff3518dcb8bea879c93c1d484a8f43abb7
beha757 <159917751+Behatotsamiy@users.noreply.github.com>
2026-03-14 23:26:50 +0500

    user entity

--- src/_auth/entities/auth.entity.ts
+++ src/_auth/entities/auth.entity.ts
@@ -1,1 +1,18 @@
-export class Auth {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('auth')
+export class Auth {
+    @Column()
+    phone: string;
+
+    @Column()
+    first_name: string;
+
+    @Column()
+    last_name: string;
+
+    @Column()
+    password: string;
+}
+   
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,33 @@
-export class CreateUserDto {}
+// src/modules/users/dto/create-user.dto.ts
+import { IsEnum, IsNotEmpty, IsPhoneNumber, IsString, MinLength, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { UserRole } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @ApiProperty({ example: '+998901234567' })
+  @IsPhoneNumber() // Проверит формат номера
+  phone: string;
+
+  @ApiProperty({ example: '12345678' })
+  @MinLength(8, { message: 'Пароль должен быть не короче 8 символов' })
+  password: string;
+
+  @ApiProperty({ example: 'Alisher' })
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @ApiProperty({ example: 'Usmanov' })
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @ApiProperty({ enum: UserRole, example: UserRole.TEACHER })
+  @IsEnum(UserRole)
+  role: UserRole;
+
+  @ApiProperty({ example: 'My IT Center', required: false })
+  @IsOptional()
+  @IsString()
+  companyName?: string;
+}
