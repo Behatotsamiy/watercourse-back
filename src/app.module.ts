417a34672073f9893f6da5a90f619d18587aa11f
beha757 <159917751+Behatotsamiy@users.noreply.github.com>
2026-06-14 19:13:01 +0500

    neon db

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,40 +19,38 @@ import { User } from './users/entities/user.entity';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        const isProd = configService.get('NODE_ENV') === 'production';
-        const databaseUrl = configService.get('DATABASE_URL');
-        if (isProd && databaseUrl) {
-          return {
-            type: 'postgres',
-            url: databaseUrl,
-            autoLoadEntities: true,
-            synchronize: true,
-            logging: true,
-            ssl: {
-              rejectUnauthorized: false,
-            },
-            uuidExtension: 'pgcrypto',
-          };
-        }
-        return {
-          type: 'postgres',
-          host: configService.get('DB_HOST'),
-          port: +configService.get('DB_PORT'),
-          username: configService.get('DB_USERNAME'),
-          password: configService.get('DB_PASSWORD'),
-          database: configService.get('DB_NAME'),
-          autoLoadEntities: true,
-          entities: [User],
-          synchronize: true,
-          logging: true,
-          uuidExtension: 'pgcrypto',
-          url: process.env.DATABASE_URL,
-          ssl: {
-    rejectUnauthorized: false, // Обязательно для Neon!
-  },
-        };
+     useFactory: (configService: ConfigService) => {
+  const databaseUrl = configService.get('DATABASE_URL');
+
+  // Если есть строка подключения (Render / Neon), работаем по ней
+  if (databaseUrl) {
+    return {
+      type: 'postgres',
+      url: databaseUrl,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+      ssl: {
+        rejectUnauthorized: false, // Критично для Neon
       },
+      uuidExtension: 'pgcrypto',
+    };
+  }
+
+  // Если строки нет (локалка), берем обычные переменные из .env
+  return {
+    type: 'postgres',
+    host: configService.get('DB_HOST') || 'localhost',
+    port: +configService.get('DB_PORT') || 5433,
+    username: configService.get('DB_USERNAME') || 'postgres',
+    password: configService.get('DB_PASSWORD') || 'root',
+    database: configService.get('DB_NAME') || 'watercourse',
+    autoLoadEntities: true,
+    synchronize: true,
+    logging: true,
+    uuidExtension: 'pgcrypto',
+  };
+},
     }),UsersModule, StudentsModule, AuthModule, GroupsModule, PaymentsModule, CoursesModule, AttendanceModule, ScheduleModule],
   controllers: [AppController],
   providers: [AppService],
