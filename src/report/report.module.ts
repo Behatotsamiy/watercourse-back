bd2a2c84e54d937e593872d864095908f203c6b4
beha757 <159917751+Behatotsamiy@users.noreply.github.com>
2026-08-11 13:57:43 +0500

    report fixed

--- src/report/report.module.ts
+++ src/report/report.module.ts
@@ -5,9 +5,10 @@ import { Attendance } from 'src/attendance/entities/attendance.entity';
 import { Payment } from 'src/payments/entities/payment.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Student } from 'src/students/entities/student.entity';
+import { SalaryModule } from 'src/salary/salary.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment, Attendance, Student])], // Добавьте необходимые сущности
+  imports: [TypeOrmModule.forFeature([Payment, Attendance, Student]), SalaryModule], // Добавьте необходимые сущности
   controllers: [ReportController],
   providers: [ReportService],
 })
